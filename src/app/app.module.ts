6d5af4e65dddf70eeffe1cbd45c9f9a45f1e1dbd
Denis Mwai <mwai.denis5@gmail.com>
2020-09-30 21:19:27 +0300

    Initial hero section UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroComponent } from './sections/hero/hero.component';
+import { GetStartedCardComponent } from './sections/hero/get-started-card/get-started-card.component';
 import { WhatWeDoComponent } from './sections/what-we-do/what-we-do.component';
 import { BenefitsComponent } from './sections/benefits/benefits.component';
 import { ClientsComponent } from './sections/clients/clients.component';
 import { DemoComponent } from './sections/demo/demo.component';
 import { FooterComponent } from './sections/footer/footer.component';
-import { GetStartedCardComponent } from './hero/get-started-card/get-started-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
 
     NavbarComponent,
+    GetStartedCardComponent,
     HeroComponent,
     WhatWeDoComponent,
     BenefitsComponent,
     ClientsComponent,
     DemoComponent,
     FooterComponent,
-    GetStartedCardComponent,
   ],
   imports: [
     BrowserModule
--- src/app/sections/hero/hero.component.ts
+++ src/app/sections/hero/hero.component.ts
@@ -9,7 +9,24 @@ export class HeroComponent implements OnInit {
 
   constructor() { }
 
+  cardsDetails = [
+    {
+      imageUrl: '../../../../assets/images/business-icon.jpg',
+      heading: 'Lipachat for business',
+      text: 'Automate your business customer care, sell and market yourself on Whatsapp.',
+      link: 'https://api.whatsapp.com/send?phone=254733919333&text=Hi'
+    },
+    {
+      imageUrl: '../../../../assets/images/user-icon.jpg',
+      heading: 'Lipachat for individuals',
+      text: 'Buy airtime, pay bills and convert bonga points to cash on Whatsapp.',
+      link: 'https://api.whatsapp.com/send?phone=254733919333&text=Hi'
+    }
+  ];
+
   ngOnInit(): void {
   }
+
+
 
 }
