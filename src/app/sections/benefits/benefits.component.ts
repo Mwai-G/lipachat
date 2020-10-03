f234eaed6917d9a560e6c35eb021694e4a136890
Kenneth Muchiri <kenmucciz8@gmail.com>
2020-10-03 23:18:25 +0300

    Added new carousel

--- src/app/sections/benefits/benefits.component.ts
+++ src/app/sections/benefits/benefits.component.ts
@@ -10,22 +10,22 @@ export class BenefitsComponent implements OnInit {
 
   benefits: IBenefit[] = [
     {
-      image: 'assets/images/save-icon.png',
+      image: 'assets/images/icons8-money-box-64.png',
       name: 'Save On Cost',
       description: 'Quickly handle customer enquiries and get feedback faster at a cheaper cost.'
     },
     {
-      image: 'assets/images/grow-icon.png',
+      image: 'assets/images/icons8-account-64.png',
       name: 'Growth',
       description: 'Easily engage with a broader customer audience from a single platform and grow your business.'
     },
     {
-      image: 'assets/images/c-support.png',
+      image: 'assets/images/icons8-online-support-64.png',
       name: 'Better Customer Service',
       description: 'Provide your customers with easy payments methods on whatsapp.'
     },
     {
-      image: 'assets/images/intergrate-icon.png',
+      image: 'assets/images/icons8-gears-64.png',
       name: 'Integration',
       description: 'Easy Intergration  to MPESA, banks and internal erp systems.'
     }
