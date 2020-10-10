59726dee1c53e7f9c2ab326ef39598b709caad9b
Denis Mwai <mwai.denis5@gmail.com>
2020-10-10 09:08:42 +0300

    Hacky demo video placement hack

--- src/app/sections/demo/demo.component.ts
+++ src/app/sections/demo/demo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 declare var Swiper: any;
 
 @Component({
@@ -11,24 +11,16 @@ export class DemoComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    var swiper = new Swiper('.swiper-demo', {
+    const swiper = new Swiper('.swiper-demo', {
       slidesPerView: 1,
-      // autoplay: true,
       loop: true,
       speed: 800,
       spacebetween: 10,
-      // navigation: {
-      //   nextEl: '.swiper-button-next',
-      //   prevEl: '.swiper-button-prev',
-      // },
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
       },
     });
 
-    let myIframe = document.getElementsByTagName('iframe')[0].contentWindow;
-    console.log(myIframe.document)
   }
-
 }
